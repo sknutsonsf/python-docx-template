6137a0967995135a37364df4534e8c23890596d7
StanKnutson <stan@powwowenergy.com>
2021-07-27 18:11:13 -0700

    Update example template and add workaround using docxcompose

--- tests/multipage_report_docxcomposes.py
+++ tests/multipage_report_docxcomposes.py
@@ -2,6 +2,13 @@
 '''
 Created : 2021-07-27
 
+This file shows the WORKAROUND for generating a multi-page report
+
+It uses the extra library docxcomposer, which should not be required
+
+There is one restriction: the template for "first page" must not want to have any content AFTER the detail pages.
+(or modify the composer call to generate and include a "trailer page")
+
 @author: Stan Knutson, stan@agmonitor.com
 '''
 
@@ -14,7 +21,7 @@ import jinja2
 
 
 DETAIL_TEMPLATE = 'templates/subdoc_detail_page.docx'
-OUTER_TEMPLATE = 'templates/subdoc_multipage_tpl.docx'
+OUTER_TEMPLATE = 'templates/subdoc_multipage_workaround.docx'
 
 IMAGE_1 = 'templates/django.png'
 IMAGE_2 = 'templates/zope.png'
@@ -29,14 +36,29 @@ specs = [
      "image_file": IMAGE_2},
 ]
 
-OUTFILE = "output/multipage_report.docx"
+TEMPDIR = "output/"
+
+OUTFILE = "output/multipage_report_workaround.docx"
 
 def gen_report():
     detail_pages = []
 
-    doc = DocxTemplate(OUTER_TEMPLATE)
+    # our report has one "front page"
+    # and many "detail pages
 
-    # build all of the inner subpages
+    doc = DocxTemplate(OUTER_TEMPLATE)
+    # generate the front page
+    context = {"customer_name": "sample",
+               #"detail_pages": detail_pages
+               "case_specs": specs
+               }
+    doc.render(context)
+    header_file = TEMPDIR + "/temp_multipage_header.docx"
+    doc.save(header_file)
+
+    files_to_merge = []
+
+    # build all of the detail pages
     for ix, spec in enumerate(specs):
         detail_doc = DocxTemplate(DETAIL_TEMPLATE)
 
@@ -53,20 +75,24 @@ def gen_report():
 
         # ALTERNATE: add to subdocument, but then the image is not actually included
 
+        #spec["case_image"] = "file " + spec["image_file"]
         ctx = dict(spec)
         ctx["case_image"] = InlineImage(detail_doc, spec["image_file"], width=Inches(6.5))
 
         detail_doc.render(context=ctx)
-        detail_subdoc = doc.new_subdoc()
-        detail_subdoc.subdocx = detail_doc.docx
-        detail_pages.append(detail_subdoc)
+        temp_file = TEMPDIR + f"temp_doc_{ix+1}.docx"
+        detail_doc.save(temp_file)
 
-    # outer page context
-    context = {"customer": "sample",
-               "case_specs": specs,
-               "detail_pages": detail_pages}
-    doc.render(context)
-    doc.save(OUTFILE)
+        files_to_merge.append(temp_file)
+
+    from docxcompose.composer import Composer
+    from docx import Document
+    master = Document(header_file)
+    composer = Composer(master)
+    for fn in files_to_merge:
+        doc1 = Document(fn)
+        composer.append(doc1)
+    composer.save(OUTFILE)
     print("Wrote", OUTFILE)
 
 if __name__ == "__main__":
